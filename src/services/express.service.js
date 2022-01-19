377b3f8dd7717c0d2d0c2fba0be4d4afa1c86d7d
eleacarton <eleacarton@me.com>
2022-01-19 15:27:28 +0100

    services and install packages

---
+++ src/services/express.service.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const apiRouter = require('../routes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+exports.start = () => {
+    
+    const port = process.env.PORT;
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use('/api/v1', apiRouter);
+
+    app.listen(port, (err) => {
+        if (err) {
+            console.log(`Error : ${err}`);
+            process.exit();
+        }
+        console.log(`app is running on port ${port}`);
+    })
+}
